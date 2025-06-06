c4d53b53c9787b3ecb7f5f4ee1aa7ccd0a18e322
@Aryan.m1 <Aryan.m@wanbuffer.in>
2025-06-06 14:24:20 +0530

    Data base

--- app/database.py
+++ app/database.py
@@ -3,7 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:yourpassword@localhost:5432/ocrdb"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+
+# Corrected database URL
+DATABASE_URL = "postgresql+psycopg2://postgres:%40Aryan9054@localhost:5432/ocr_saas"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
